4cb26b96cdaacfd4e395abb1cf0945b7e76a0ce2
kelvinrandu <randukelvin@gmail.com>
2018-07-19 01:03:44 +0300

    [Feature#159129933]/ Get all diary entries enpoint

--- app-test.py
+++ app-test.py
@@ -10,6 +10,12 @@ class BasicTestCase(unittest.TestCase):
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Hello, World!')
+        
+#test for get all entries
+    def test_get_all(self):
+        tester = app.test_client(self)
+        response = tester.get('/api/v1/entries', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
