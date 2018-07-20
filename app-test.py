37fa270fa3b53d864ea65b990f3061cef9a255bf
kelvinrandu <randukelvin@gmail.com>
2018-07-20 17:17:54 +0300

    [feature#159130333]/Delete an entry  endpoint

--- app-test.py
+++ app-test.py
@@ -12,6 +12,14 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(response.data, b'Hello, World!')
 
 
+#delete entry endpoint test
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.delete('/api/v1/entries/<int:id>', content_type='html/text')
+       # self.assertEqual(response.status_code, 200)
+        self.assertEquals(response.json, dict(success=True))
+
+
 if __name__ == '__main__':
     unittest.main()
     
--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
+
+
+#Dictionary to temporily store/hold diary entries 
+Entries = [
+		{
+		 	'id': 1,
+			'title':' Article one',
+			'body':'This represents the body of the first article',
+			'create_date':'04-25-2018'
+		},
+		{
+			'id': 2,
+			'title':' Article two',
+			'body':'This represents the body of the second article',
+			'create_date':'04-25-2018'
+		},
+		{
+		 	'id': 3,
+			'title':' Article three',
+			'body':'This represents the body of the third article',
+			'create_date':'04-25-2018'
+		}
+
+	]
 #test route
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+#delete entry route
+@app.route('/api/v1/entries/<int:id>', methods=['DELETE'])
+def delete_entry(id):
+    entry = [entry for entry in Entries if entry['id'] == id]
+    Entries.remove(entry[0])
+    return jsonify({ "status" : "true" })
 
 if __name__ == '__main__':
     app.run(debug=True)
