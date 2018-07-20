f1e47d2ecba6a52bbde46291723532a0c520f44b
kelvinrandu <randukelvin@gmail.com>
2018-07-20 11:30:04 +0300

    [feature#159130175]/ Added create entry endpoint

--- app-test.py
+++ app-test.py
@@ -11,6 +11,13 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Hello, World!')
 
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 201)
+        
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 #test route
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+#post entry endpoint
+@app.route('/api/v1/entries', methods=['POST'])
+def post_entry():
+    data = request.get_json(["data"])
+    entry = data
+    return jsonify({ 'status': 'entry created successfully'}), 201
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
